2a00c959b68a2b12a0f25428b505b42ea54a626f
ovrsa <sufferin.in@gmail.com>
2024-04-13 13:08:05 +0900

    add:gnome sort

--- algo/sort.py
+++ algo/sort.py
@@ -1,3 +1,20 @@
+import logging
+
+# ロガーの設定
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# ログの出力先（ハンドラ）を標準出力に設定
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+# ログのフォーマットを設定
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# ロガーにハンドラを追加
+logger.addHandler(handler)
+
 """Buuble Sort"""
 def bubble_sort(arr):
     n = len(arr)
@@ -20,5 +37,28 @@ def quick_sort(arr):
 
 
 """Gnome Sort"""
-def gnome_sort(arr):
-    print("Gnome Sort")
+def gnome_sort(arr: list):
+    n = len(arr)
+    logger.debug(n)
+    index = 0
+    while index < n:
+        logger.debug('+++++')
+        logger.debug(f'arr: {arr}')
+        # 比較対象のラインが0の場合は1にする
+        if index == 0:
+            index += 1
+        logger.debug(f'arr{index}: {arr[index]}, arr{index-1}: {arr[index-1]}')
+        # 値を入れ替えてindexに1を足す
+        logger.debug(f'index: {index}')
+        if arr[index] >= arr[index - 1]:
+            logger.debug('pass')
+            index += 1
+        # indexとindex-1の値を入れ替える
+        else:
+            arr[index], arr[index- 1] = arr[index-1], arr[index]
+            logger.debug('check')
+            logger.debug(f'arr{index}: {arr[index]}, arr{index-1}: {arr[index-1]}')
+            # indexから1を引く
+            index -= 1
+        logger.debug('-----')
+    return arr
--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from algo import sort
+from algo.sort import *
 
-arr = [64, 34, 25, 12, 22, 11, 90]
+# テストケースと期待される結果をタプルのリストとして定義
+test_cases = [
+    ([64, 34, 25, 12, 22, 11, 90], [11, 12, 22, 25, 34, 64, 90]),
+    ([5, 1, 4, 2, 8], [1, 2, 4, 5, 8]),
+    ([], []),  # 空のリストのケース
+]
 
-
-def app(arr):
-    sorted_arr = sort.gnome_sort(arr)
-    assert sorted_arr == [11, 12, 22, 25, 34, 64, 90]
-    
+# 各ソート関数をテストするための関数を定義
+def run_sort_tests(sort_func):
+    for input_arr, expected in test_cases:
+        assert sort_func(input_arr) == expected, f"{sort_func.__name__} failed"
 
 if __name__ == '__main__':
-    app(arr)
+    # 各ソート関数に対してテストを実行
+    # run_sort_tests(bubble_sort)
+    # run_sort_tests(quick_sort)
+    run_sort_tests(gnome_sort)
+    print('All tests passed!')
