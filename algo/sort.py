4a41b8aecab5fd21e678b344189b9f4e0dc00d1c
ovrsa <sufferin.in@gmail.com>
2024-04-14 23:27:51 +0900

    insert sort

--- algo/sort.py
+++ algo/sort.py
@@ -51,16 +51,25 @@ def gnome_sort(arr: list):
 """Insertion Sort"""
 def insertion_sort(arr: list[int]) -> list[int]:
     len_numbers = len(arr)
+    logger.debug(f'Starting insertion sort on array: {arr}')
     # 0番目の要素は比較対象がないため1から始める
     for i in range(1, len_numbers):
+        logger.debug(f'Iteration {i}: Current array state: {arr}')
         # 比較対象の値をtempに代入
         temp = arr[i]
+        logger.debug(f'Picking up value {temp} for insertion')
         # 比較対象はi-1から始める
         j = i - 1
         # jが0以上かつ、arr[j]の値がtempより大きい場合
         while j >= 0 and arr[j] > temp:
-            # jの値がtempより大きい場合、j+1にjの値を代入
+            logger.debug(f'Comparing {temp} with {arr[j]} at position {j}')
+            # j+1にjの値を代入
             arr[j + 1] = arr[j]
+            logger.debug(f'Moving {arr[j]} to position {j + 1}')
+            # jを1減らすことで次の比較対象に移る
             j -= 1
+        # 最後にtempの値をarr[j+1]に代入することで、挿入を完了する
         arr[j + 1] = temp
+        logger.debug(f'Inserted {temp} at position {j + 1}, array state: {arr}')
+    logger.debug(f'Finished insertion sort, final array state: {arr}')
     return arr
